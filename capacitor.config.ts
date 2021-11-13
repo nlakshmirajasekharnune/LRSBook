44f013e59c315a5dc17f620815a8bbfab9db03d3
nlakshmirajasekarnune <rajasekhar.nune@bitapps.com>
2021-11-13 06:09:02 +0530

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'LRSBook',
+  webDir: 'www',
+  bundledWebRuntime: false
+};
+
+export default config;
